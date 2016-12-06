e3b81d146c08dfcb6f7111b08df92c4f53b73874
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-06 23:16:47 +0000

    composing the log message

--- logger.js
+++ logger.js
@@ -6,6 +6,8 @@ module.exports = function (req, res, next) {
 
   res.on('finish', function() {
     var duration = +new Date() - start;
+    var message = `${method} to ${url} \ntook ${duration} ms \n\n`;
+    
   });
   next();
 }
