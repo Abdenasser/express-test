f0e7c36b57d79263dae9692533900b96b9c9b5c3
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-06 22:35:31 +0000

    adding static middleware serving files from the public folder

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('public'));
 
 app.listen(3000);
