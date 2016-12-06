fdd6f135762fa32948cf2be7fb6208e737a08567
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-06 22:56:55 +0000

    responding with JSON

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/blocks', function(req, res) {
+  var blocks = ['Fixed', 'Movable', 'Rotating'];
+  res.json(blocks)
+});
+
 app.listen(3000);
