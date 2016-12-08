dbf10360c267e04019eab8a98d1bf3fc75c95da3
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-08 20:34:49 +0000

    Expanding our blocks list

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 var app = express();
 
+var blocks = {
+  'Fixed': 'Fastened securely in position',
+  'Movable': 'Capable of being moved',
+  'Rotating': 'Moving in a circle around its center'
+};
+
+
 var logger = require('./logger');
 app.use(logger);
 
 app.use(express.static('public'));
 
 app.get('/blocks', function(req, res) {
-  var blocks = ['Fixed', 'Movable', 'Rotating'];
   if (req.query.limit >= 0) {
     res.json(blocks.slice(0, req.query.limit));
   } else {
